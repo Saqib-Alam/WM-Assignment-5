293de8a4ef2a5e3d8214c35ceee5c94f63403b14
Saqib-Alam <saqibalam64@gmail.com>
2020-06-11 00:54:53 +0500

    WM-Assignment-5

--- app.js
+++ app.js
@@ -10,17 +10,21 @@
 // //Qno 3. Write a script to display 
 // following message on your web page: (Hint : Use line break)
 
-// alert("Welcome to JS Land... \nHappy Coding!");
+// alert("Welcome to JS Land... </br>Happy Coding!");
 
 //Qno 4. Write a script to display following messages in sequence:
 
 // alert("Welcome to JS Land... ");
-// alert("Happy coding");
+// alert("</br>Happy coding");
 
 // // Qno 5. Generate the following message through browser’s developer console:
 
 // console.log("Hello.. I can run JS through my web browser's console.");
 
+
+// Qno 6. Make use of alerts in your new/existing HTML & CSS project.
+
+alert("Stay Home Stay Safe")
 
 
 
